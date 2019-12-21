eb757c44e4110768c76349a94a42d0ad8368f977
hoodielive <hoodielive@gmail.com>
2019-12-21 16:59:01 -0500

    Initial

---
+++ url_shortener/url_shortener/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+using System.Linq;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Microsoft.AspNetCore.Mvc;
+using url_shortener.App;
+
+namespace url_shortener.Controllers
+{
+    public class URLReponse
+    {
+        public string url { get; set; }
+        public string status { get; set; }
+        public string token { get; set; }
+    }
+    
+    public classj HomeController : Controller
+    {
+         // Index Route 
+        [HttpGet, Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    
+        [HttpPost, Route("/")]
+        {
+            public IActionResult PostURL([FromBody] string url)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        // Our Redirect Route
+        [HttpGet, Route("/{token}")]
+        public IActionResult NixRedirect([FromRoute] string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
